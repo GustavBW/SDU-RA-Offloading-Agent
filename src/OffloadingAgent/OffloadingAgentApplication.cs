1ad41c6cf0661c72563ccd0848f2830624404c99
GustavBW <gustavbw@gmail.com>
2024-01-13 21:57:13 +0100

    Core Project Initialized

---
+++ src/OffloadingAgent/OffloadingAgentApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OffloadingAgent;
+
+public class OffloadingAgentApplication
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("RA-OA: Starting Application");
+        
+        //code here
+        
+        Console.WriteLine("RA-OA: Application shutdown");
+    }
+}
