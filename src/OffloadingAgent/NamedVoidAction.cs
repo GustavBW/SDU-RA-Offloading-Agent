50483124adab5b2f50ca8ef943016f6755e6c96f
GustavBW <gustavbw@gmail.com>
2024-01-13 23:23:59 +0100

    Testing

--- src/OffloadingAgent/NamedVoidAction.cs
+++ src/OffloadingAgent/NamedVoidAction.cs
@@ -4,12 +4,12 @@ namespace OffloadingAgent;
 
 public class NamedVoidAction
 {
-    public string Name { get; }
-    public Action Action { get; }
-
     public NamedVoidAction(string name, Action action)
     {
         Name = name ?? throw new ArgumentNullException(nameof(name));
         Action = action ?? throw new ArgumentNullException(nameof(action));
     }
+
+    public string Name { get; }
+    public Action Action { get; }
 }
--- src/OffloadingAgent/OffloadingAgentApplication.cs
+++ src/OffloadingAgent/OffloadingAgentApplication.cs
@@ -5,26 +5,26 @@ namespace OffloadingAgent;
 
 public class OffloadingAgentApplication
 {
+    private static readonly List<NamedVoidAction> onShutdownDo = new();
 
-    private static readonly List<NamedVoidAction> onShutdownDo = new List<NamedVoidAction>();
-    
     public static void addShutdownHook(NamedVoidAction func)
     {
         onShutdownDo.Add(func);
     }
-    
-    
-    static void Main(string[] args)
+
+
+    private static void Main(string[] args)
     {
         Console.WriteLine("RA-OA: Starting Application");
         addShutdownHook(new NamedVoidAction("Func1", () => Console.WriteLine("I'm a hooker")));
         //code here
-        
-        foreach (NamedVoidAction action in onShutdownDo)
+
+        foreach (var action in onShutdownDo)
         {
             Console.WriteLine("Executing: " + action.Name);
             action.Action();
         }
+
         Console.WriteLine("RA-OA: Application shutdown");
     }
 }
