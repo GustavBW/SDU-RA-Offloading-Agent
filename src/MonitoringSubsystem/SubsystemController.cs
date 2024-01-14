ecc3d3c0f7eceb60a79c34f749a4c621263fc723
GustavBW <gustavbw@gmail.com>
2024-01-14 13:17:34 +0100

    Preparing Subsystem

--- src/MonitoringSubsystem/SubsystemController.cs
+++ src/MonitoringSubsystem/SubsystemController.cs
@@ -1,13 +1,44 @@
 ﻿using System;
+using System.Threading;
 using Monitoring;
 
 namespace MonitoringSubsystem;
 
 public class SubsystemController
 {
+    private static Thread subsystemThread;
+    private static SubsystemController instance;
+    
+    /*
+     * Spawns a new thread, and initializes a subsystem on this thread.
+     * Don't call this method more than once, since the instance returned by GetInstance() will be replaced each time. 
+     */
     public static void Init()
     {
         Console.WriteLine("[MSC] Initizalizing ");
         Console.WriteLine("[MSC] Using Monitoring version: " + Monitoring.ModuleInfo.VERSION);
+
+        subsystemThread = new Thread(() => new SubsystemController().Start());
+        subsystemThread.Start();
+        subsystemThread.Join();
+    }
+
+    /**
+     * May return null
+     */
+    public static SubsystemController GetInstance()
+    {
+        return instance!;
+    }
+
+    private void Start()
+    {
+        instance = this;
+        Console.WriteLine("[MSC] SubsystemThread is: " + subsystemThread + "#" + subsystemThread.GetHashCode());
+        Console.WriteLine("[MSC] Subsystem instance is: " + instance + "#" + instance.GetHashCode());
+        
+        
+        
+        
     }
 }
---
+++ src/MonitoringSubsystem/ThreadSafeValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MonitoringSubsystem;
+
+public class ThreadSafeValue<T>
+{
+    private T _value;
+    private readonly object _mutex = new();
+
+    public ThreadSafeValue(T value)
+    {
+        this._value = value;
+    }
+
+    public T Get()
+    {
+        lock (_mutex)
+        {
+            return _value;
+        }
+    }
+
+    public void Set(T value)
+    {
+        lock (_mutex)
+        {
+            this._value = value;
+        }
+    }
+}
